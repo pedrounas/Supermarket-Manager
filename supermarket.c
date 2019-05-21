a2bc42640c7ea913b9e51f1660dc149048d764ff
Pedro Unas <shockwave2k11@gmail.com>
2019-05-21 18:16:43 +0100

    Change helper and simulation function to match Python program

--- supermarket.c
+++ supermarket.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <limits.h>
+#include <stdlib.h>
+#include <time.h>
 
 #include "queue.h"
 #include "client.h"
@@ -26,6 +28,8 @@ void showCashiers(Cashier **list)
     for (int i = 0; i < size; i++)
     {
         printCashier(list[i]);
+        if (length(list[i]->queue) > 1)
+            exit(0);
     }
 }
 
@@ -46,15 +50,13 @@ void handleFirst(int i, Cashier *cashier)
     Client *c = first(cashier->queue);
     int waiting = i - getEta(cashier);
     int itemsProcessed = waiting * getSpeed(cashier);
-
     if (itemsProcessed >= items(c))
     {
         updateEta(cashier, (i + 1));
         updateProducts(cashier, items(c));
-        addClient(cashier);
         updateWaitingTime(cashier, (i - entrance(c)));
         removeClient(cashier);
-        printf("Client went through cashier %c\n", getId(cashier));
+        printf("Client went through cashier %d\n", getId(cashier));
     }
 }
 
@@ -62,10 +64,12 @@ void moveClients(int i, Cashier **list)
 {
     for (int j = 0; j < size; j++)
     {
-        if (isEmpty(list[j]))
+        if (isEmpty(list[j]) == TRUE)
+        {
             updateEta(list[j], i);
+        }
         else
-            handleFirst(i, list[i]);
+            handleFirst(i, list[j]);
     }
 }
 
@@ -117,24 +121,18 @@ void simulation(int rate, int productRate, int numberOfCashiers, int numberOfTur
     {
         printf("Cycle %d\n", i);
 
-        moveClients(i, list);
-
-        int random = randomNumber();
-
+        int random = randomNumber_();
         if (random <= (int)rate / 100)
         {
             int numberOfProducts = randomNumber();
-            Client *c = mk_client(i*4,i, numberOfProducts);
-
+            Client *c = mk_client(i * 4, numberOfProducts * 2, i);
             printf("New client with %d products entered at %d\n", numberOfProducts, i);
-
             int cashierId = chooseCashier(list);
             int itemList = items(c);
-            enqueue(c, list[cashierId]->queue);
-            updateProducts(list[cashierId], itemList);
+            addClient(list[cashierId], c);
         }
-
         showCashiers(list);
+        moveClients(i, list);
     }
 
     printf("Simulation ended\n");
@@ -142,7 +140,7 @@ void simulation(int rate, int productRate, int numberOfCashiers, int numberOfTur
     i++;
     while (emptyCashiers(list) == TRUE)
     {
-        printf("Cycle %d\n", i);
+        printf("\nCycle %d\n", i);
         moveClients(i, list);
         showCashiers(list);
         i++;
@@ -153,6 +151,8 @@ void simulation(int rate, int productRate, int numberOfCashiers, int numberOfTur
 
 int main()
 {
+    srand(time(NULL));
+
     int rate, productRate, numberOfCashiers, numberOfTurns;
     printf("Insira a afluência, apetencia, numero de caixas e numero de ciclos: ");
     scanf("%d %d %d %d", &rate, &productRate, &numberOfCashiers, &numberOfTurns);
